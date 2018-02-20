341eb9562a2ac9eb0b9a944e602e06cc80f76b4a
wartgowj <wartgowj@gmail.com>
2018-02-19 20:05:35 -0800

    created first files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheese Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheese Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", false);
