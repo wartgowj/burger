40f5660a88f678a3c325d2b6a3fd3b4f2cca8047
wartgowj <wartgowj@gmail.com>
2018-02-22 18:20:15 -0800

    added public folder

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(express.static("public"));
+
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
